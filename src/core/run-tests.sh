b14b32707de16043a44af7668d5e1a8f7593c982
John Novak <john@johnnovak.net>
2017-06-11 08:43:36 +1000

    Add test runner to core

---
+++ src/core/run-tests.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function execute_test {
+  echo
+  set -x
+  nim c -r $@
+  if [[ $? != 0 ]]; then exit $?; fi
+  set +x
+}
+
+execute_test core
+execute_test -d:useFloat64 core
+
+execute_test geometry
+execute_test -d:useFloat64 geometry
+
+execute_test transform
+execute_test -d:useFloat64 transform
